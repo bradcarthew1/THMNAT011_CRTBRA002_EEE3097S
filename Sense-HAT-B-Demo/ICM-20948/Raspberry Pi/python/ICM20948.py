e563d64400453e62f7f2cb503e85eff407c23448
nathanael47 <thmnat011@myuct.ac.za>
2021-10-22 14:48:52 +0100

    added demo files for sensor, and new files

--- Sense-HAT-B-Demo/ICM-20948/Raspberry Pi/python/ICM20948.py
+++ Sense-HAT-B-Demo/ICM-20948/Raspberry Pi/python/ICM20948.py
@@ -379,7 +379,10 @@ if __name__ == '__main__':
   print("\nSense HAT Test Program ...\n")
   MotionVal=[0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
   icm20948=ICM20948()
-  while True:
+  #while True:
+  print("roll		pitch		yaw		AccX	AccY	AccZ	GyrX	GyrY	GyrZ	MagX	MagY	MagZ")
+  for i in range(20):
+    time.sleep(1)
     icm20948.icm20948_Gyro_Accel_Read()
     icm20948.icm20948MagRead()
     icm20948.icm20948CalAvgValue()
@@ -390,11 +393,13 @@ if __name__ == '__main__':
     pitch = math.asin(-2 * q1 * q3 + 2 * q0* q2)* 57.3
     roll  = math.atan2(2 * q2 * q3 + 2 * q0 * q1, -2 * q1 * q1 - 2 * q2* q2 + 1)* 57.3
     yaw   = math.atan2(-2 * q1 * q2 - 2 * q0 * q3, 2 * q2 * q2 + 2 * q3 * q3 - 1) * 57.3
-    print("\r\n /-------------------------------------------------------------/ \r\n")
-    print('\r\n Roll = %.2f , Pitch = %.2f , Yaw = %.2f\r\n'%(roll,pitch,yaw))
-    print('\r\nAcceleration:  X = %d , Y = %d , Z = %d\r\n'%(Accel[0],Accel[1],Accel[2]))  
-    print('\r\nGyroscope:     X = %d , Y = %d , Z = %d\r\n'%(Gyro[0],Gyro[1],Gyro[2]))
-    print('\r\nMagnetic:      X = %d , Y = %d , Z = %d'%((Mag[0]),Mag[1],Mag[2]))
+    print("\n"+str(roll)+"	"+str(pitch)+"	"+str(yaw)+"	"+str(Accel[0])+"	"+str(Accel[1])+"	"+str(Accel[2])+"	"+str(Gyro[0])+"	"+str(Gyro[1])+"	"
+    +str(Gyro[2])+"	"+str(Mag[0])+"	"+str(Mag[1])+"	"+str(Mag[2]));
+    #print("\r\n /-------------------------------------------------------------/ \r\n")
+    #print('\r\n Roll = %.2f , Pitch = %.2f , Yaw = %.2f\r\n'%(roll,pitch,yaw))
+    #print('\r\nAcceleration:  X = %d , Y = %d , Z = %d\r\n'%(Accel[0],Accel[1],Accel[2]))  
+    #print('\r\nGyroscope:     X = %d , Y = %d , Z = %d\r\n'%(Gyro[0],Gyro[1],Gyro[2]))
+    #print('\r\nMagnetic:      X = %d , Y = %d , Z = %d'%((Mag[0]),Mag[1],Mag[2]))
     
 
 
